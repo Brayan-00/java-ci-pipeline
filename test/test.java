c22e31a3c83ca7f8ad8584abfe6281c444352947
Brayan-00 <brayanfernando.10@gmail.com>
2020-03-14 18:03:28 -0600

    [Modify] build.xml

--- test/test.java
+++ test/test.java
@@ -1,9 +1,9 @@
-package test;
 import main.calculator;
-import org.junit.*;
-import java.util.*;
-
+import org.junit.Before;
+import org.junit.jupiter.api.*;
+import java.util.Random;
 import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class test {
 
@@ -30,7 +30,7 @@ public class test {
         int c = random();
         int d = random();
         assertEquals(calc.minus(a, b), a - b);
-        assertEquals(calc.add(c, d), c - d);
+        assertEquals(calc.minus(c, d), c - d);
     }
     @Test
     public void testMultiply()
@@ -40,7 +40,7 @@ public class test {
         int c = random();
         int d = random();
         assertEquals(calc.multiply(a, b), a * b);
-        assertEquals(calc.add(c, d), c * d);
+        assertEquals(calc.multiply(c, d), c * d);
     }
     @Test
     public void testDivide()
@@ -50,15 +50,16 @@ public class test {
         int c = random();
         int d = random();
         assertEquals(calc.divide(a, b), a/b);
-        assertEquals(calc.add(c, d), c/d);
+        assertEquals(calc.divide(c, d), c/d);
     }
-    @Test(expected=IllegalArgumentException.class)
+    @Test
     public void testDivideZero() {
         int a = random();
         int b = 0;
         int d = random();
-        calc.divide(a,b);
-        calc.divide(d,b);
+        assertThrows(IllegalArgumentException.class,() -> {calc.divide(a,b); });
+        assertThrows(IllegalArgumentException.class,() -> {calc.divide(d,b); });
+
     }
 
 
